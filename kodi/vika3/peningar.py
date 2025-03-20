32fc5295e3667c1a46eb6da9db92cb03838d209d
einardarri <git@einardarri.is>
2025-03-20 15:05:35 +0000

    kodi

---
+++ kodi/vika3/peningar.py
@@ -0,0 +1,15 @@
+peningur = int(input())
+
+tiuk = peningur//10000
+peningur = peningur % 10000
+
+fimmk = peningur//5000
+peningur = peningur % 5000
+
+thusund = peningur//1000
+peningur = peningur % 1000
+
+print("10k seðlar",tiuk)
+print("5k seðlar",fimmk)
+print("1k seðlar", thusund)
+print("Afgangs", peningur)
