5b9922f59f3ed94d630553cd871dc41736e05b5d
einardarri <git@einardarri.is>
2025-03-20 15:58:35 +0000

    forgot to save lol ;)

--- kodi/vika3/oddatala.py
+++ kodi/vika3/oddatala.py
@@ -10,3 +10,8 @@ if tala > tala2:
     print("fyrri talan er stæri")
 elif tala < tala2:
     print("Seinni talan er stæri")
+else:
+    print("Þær eru jafn stórar")
+
+if "a" in "abc":
+    print("a er partur af streinginum")
